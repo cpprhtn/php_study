cbee88ff74c3025b88145525d68f6e132566c866
cpprhtn <cpprhtn@naver.com>
2022-09-30 21:42:27 +0900

    add: PHP의 변수

---
+++ Sites/variable.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<h1>Variable</h1>
+<?php
+$a = 1;
+echo $a+1;
+?>
+
+<?php
+$name = "ResNeSt";
+echo "While image classification models have recently continued to advance, most downstream applications such as object detection and semantic segmentation still employ ResNet variants as the backbone network due to their simple and modular structure. We present a simple and modular Split-Attention block that enables attention across feature-map groups. By stacking these Split-Attention blocks ResNet-style, we obtain a new ResNet variant which we call ".$name.". Our network preserves the overall ResNet structure to be used in downstream tasks straightforwardly without introducing additional computational costs. ".$name." models outperform other networks with similar model complexities, and also help downstream tasks including object detection, instance segmentation and semantic segmentation.";
+?>
+
+</body>
+</html>
