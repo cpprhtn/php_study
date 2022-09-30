5f0f55b36113b18d9df519be3d4b4c20893d8c38
cpprhtn <cpprhtn@naver.com>
2022-09-30 21:43:09 +0900

    add: PHP의 URL 파라미터

---
+++ Sites/url_parameter.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+<body>
+	안녕하세요. egoing님<br>
+	안녕하세요. <?php echo $_GET['name']; ?>님
+	안녕하세요. <?php echo $_GET['address']; ?>에 사시는 <?php echo $_GET['name']; ?>님
+</body>
+</html>
+
+<!-- http://localhost/url_parameter.php?name=준원&address=부산 -->
