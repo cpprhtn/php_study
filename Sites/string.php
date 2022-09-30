a51a32ff7aad4509be6a7c39242ae3b3304461b8
cpprhtn <cpprhtn@naver.com>
2022-09-30 21:42:13 +0900

    add: PHP와 문자열

---
+++ Sites/string.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<body>
+	<h1>String & String Operator</h1>
+<?php
+echo 'Hello word';
+echo "Hello word";
+echo 'Hello "w"ord';
+echo "Hello \"w\"ord";
+?>
+	<h2>concatenation operator</h2>
+<?php
+echo "Hello "."world";
+?>
+	<h2>String lenght function</h2>
+<?php
+echo strlen("Hello world");
+?>
+</body>
+</html>
