f56e928b4ed3c78d2ff1877322b9a4eb19edbfba
cpprhtn <cpprhtn@naver.com>
2022-10-02 22:28:59 +0900

    add: 글 삭제

--- Sites/index.php
+++ Sites/index.php
@@ -66,6 +66,12 @@ function print_list(){
         <?php if(isset($_GET['id'])){ ?>
             <!-- <a href="update.php?id=<?php //echo $_GET['id']; ?>">update</a> -->
             <a href="update.php?id=<?=$_GET['id']?>">update</a>
+
+            <form action="delete_process.php" method="post">
+                <input type="hidden" name="id" value="<?=$_GET['id']?>">
+                <input type="submit" value="delete">
+            </form>
+
         <?php } ?>
 
         <h2>
